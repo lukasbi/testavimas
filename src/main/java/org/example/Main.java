b5ef3f922c16adb35ba2f237f1543f3639a30841
lukasbi <lukasbistrickas@gmail.com>
2022-11-10 11:27:55 +0200

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,41 @@
-package org.example;
+package ivadas;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+    @Test
+    public void testSumResultProvidingTwoAndTwo() {
+        //1. Pateikiam duomenis
+        int variableA = 2;
+        int variableB = 2;
+        int expectedResult = 4;
+        int actualResult;
+
+        //2. Atliekame testo veiksmus
+        actualResult = variableA + variableB;
+
+        //3. Palyginam rezultatus
+        Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Test
+    public void testDifferenceResultProvidingMinusOneAndMinusTwo() {
 
+        //1. Pateikiam duomenis
+        int variableA = -1;
+        int variableB = -2;
+        int expectedResult = -3;
+        int actualResult;
+
+        //2. Atliekame testo veiksmus
+        actualResult = variableA + variableB;
+
+        //3. Palyginam rezultatus
+        Assert.assertTrue(
+                expectedResult == actualResult,
+                String.format("Actual: %s, Expected: %s", actualResult, expectedResult)
+        );
+    }
 }
